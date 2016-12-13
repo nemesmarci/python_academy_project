a67c2181fc656e3c035169ddd5db66fdac649a16
nemesmarci <malu5555@gmail.com>
2016-12-13 21:38:49 +0100

    added storage_utils package

---
+++ storage_utils/storage_utils.py
@@ -0,0 +1,27 @@
+"""Various utils for making file-based data management easier."""
+
+import os
+
+
+def get_user_ids(storage_path):
+    """Returns user ids in the path"""
+    existing_ids = os.listdir(storage_path)
+    integer_ids = []
+    if existing_ids:
+        for i in existing_ids:
+            try:
+                current_id = int(i)
+                integer_ids.append(current_id)
+            except ValueError:
+                pass
+        return integer_ids
+    return None
+
+
+def get_next_id(storage_path):
+    """Calculate the next available identifier."""
+    integer_ids = get_user_ids(storage_path)
+    if integer_ids:
+        last_id = max(integer_ids)
+        return last_id + 1
+    return 0
