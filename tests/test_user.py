86072e69b5a92738d66b3d1e7ef14208a29ab964
nemesmarci <malu5555@gmail.com>
2016-12-13 15:49:33 +0100

    added users package added test cases for User class

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,7 +1,30 @@
 import unittest
+from users.user import User
+from datetime import date
 
 
 class TestUser(unittest.TestCase):
     """Test the user class"""
 
-    pass
+    def test_create_user(self):
+        first_name = "User1"
+        family_name = "Test"
+        birth = date(1990,1,1)
+        email = "user1@test.org"
+        testuser = User(first_name, family_name, birth, email, "secret")
+        self.assertTrue(testuser)
+        self.assertEqual(testuser.family_name, family_name)
+        self.assertEqual(testuser.first_name, first_name)
+        self.assertEqual(testuser.birth, birth)
+        self.assertEqual(testuser.email, email)
+        self.assertEqual(testuser.password, "secret")
+
+    def test_create_user_wrong_email(self):
+        self.assertRaises(ValueError, User, "User3", "Test", date(2016,12,12), "not_an.email@address", "secret")
+
+    def test_create_user_wrong_birth_date(self):
+        self.assertRaises(TypeError, User, "User3", "Test", "1", "user3@test.org", "secret")
+
+    def test_create_user_empty_field(self):
+        self.assertRaises(ValueError, User, "", "Test", date(2016,1,1), "unknown@test.org", "secret")
+
