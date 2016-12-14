4c4190584f5a86e393560371842f2174f7e3f1f3
nemesmarci <malu5555@gmail.com>
2016-12-14 10:06:44 +0100

    added new test cases

--- tests/test_user_roles.py
+++ tests/test_user_roles.py
@@ -1,7 +1,12 @@
 import unittest
+from roles.role import Role
 
 
 class TestUserRoles(unittest.TestCase):
     """Test the user role functions"""
+    def test_valid_role(self):
+        test_role = Role('admin')
+        self.assertTrue(test_role._role == 'admin')
 
-    pass
+    def test_invalid_role(self):
+        self.assertRaises(ValueError, Role, 'auditor')
